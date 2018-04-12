61612145cd33d6fc70aff5e443c288d26a31a378
Helge Ohliger <helge.ohliger@provinzial.com>
2018-04-12 11:47:01 +0200

    alter db

--- worblehat-domain/src/main/resources/de/codecentric/psd/worblehat/liquibase-changesets/version-1.0/001-initialize-db.sql
+++ worblehat-domain/src/main/resources/de/codecentric/psd/worblehat/liquibase-changesets/version-1.0/001-initialize-db.sql
@@ -25,9 +25,9 @@ CREATE TABLE borrowing(
 -- changeset action:insert_demo_data
 INSERT INTO book(title, author, edition, isbn, year_of_publication)
 VALUES
-  ("Harry Potter and the Philisopher's Stone", "J.K. Rowling", "", "0747532699", 1997, ""),
-  ("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", "", "0747542155", 1999, ""),
-  ("Harry Potter and the Goblet of Fire", "J.K. Rowling", "", "074754624X", 2000, ""),
-  ("Harry Potter and the Order of the Phoenix", "J.K. Rowling", "", "0747551006", 2003, ""),
-  ("Harry Potter and the Half-Blood Prince", "J.K. Rowling", "", "0747581088", 2005, ""),
-  ("Harry Potter and the Deathly Hallows", "J.K. Rowling", "", "0545010225", 2007, "");
+  ("Harry Potter and the Philisopher's Stone", "J.K. Rowling", "", "0747532699", 1997),
+  ("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", "", "0747542155", 1999),
+  ("Harry Potter and the Goblet of Fire", "J.K. Rowling", "", "074754624X", 2000),
+  ("Harry Potter and the Order of the Phoenix", "J.K. Rowling", "", "0747551006", 2003),
+  ("Harry Potter and the Half-Blood Prince", "J.K. Rowling", "", "0747581088", 2005),
+  ("Harry Potter and the Deathly Hallows", "J.K. Rowling", "", "0545010225", 2007);
