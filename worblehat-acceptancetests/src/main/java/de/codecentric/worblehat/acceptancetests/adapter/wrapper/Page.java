378b20a5c6e685871261fdcfb9efdac2cc4ab26a
Helge Ohliger <helge.ohliger@provinzial.com>
2018-04-13 11:15:48 +0200

    merge

--- worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/adapter/wrapper/Page.java
+++ worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/adapter/wrapper/Page.java
@@ -1,16 +1,18 @@
 package de.codecentric.worblehat.acceptancetests.adapter.wrapper;
 
 public enum Page {
-    BOOKLIST("bookList"), INSERTBOOKS("insertBooks"), BORROWBOOK("borrow"), RETURNSINGLEBOOK("returnSingleBook"),
+    BOOKLIST("bookList"),
+    INSERTBOOKS("insertBooks"),
+    BORROWBOOK("borrow"),
     RETURNBOOKS("returnAllBooks");
 
     private String url;
 
-    Page(String url) {
+    Page(String url){
         this.url = url;
     }
 
-    public String getUrl() {
+    public String getUrl(){
         return url;
     }
 }
