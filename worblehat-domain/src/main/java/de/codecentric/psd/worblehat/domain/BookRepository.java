40eae6a9646ab3001fed4878dd527cdc4d845b7b
Helge Ohliger <helge.ohliger@provinzial.com>
2018-04-13 12:15:04 +0200

    deleteByIsbn

--- worblehat-domain/src/main/java/de/codecentric/psd/worblehat/domain/BookRepository.java
+++ worblehat-domain/src/main/java/de/codecentric/psd/worblehat/domain/BookRepository.java
@@ -1,12 +1,9 @@
 package de.codecentric.psd.worblehat.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 
@@ -20,5 +17,5 @@ public interface BookRepository extends JpaRepository<Book, Long> {
 
     Set<Book> findByEdition(String edition);
 
-    void deleteBook(Book book);
+    void deleteByIsbn(String isbn);
 }
